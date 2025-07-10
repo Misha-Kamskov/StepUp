ad642536483fadc565ca70e66a7dce75ae1ed131
Misha Kamskov <143383894+Misha-Kamskov@users.noreply.github.com>
2025-07-10 21:21:13 +0300

    initial: add core and feature modules, add some auth screens

--- app/src/main/java/com/mkam/stepup/MainActivity.kt
+++ app/src/main/java/com/mkam/stepup/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.mkam.stepup.ui.theme.StepUpTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,21 +17,5 @@ class MainActivity : ComponentActivity() {
 
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StepUpTheme {
-        Greeting("Android")
     }
 }
